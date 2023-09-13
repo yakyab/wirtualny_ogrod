98b290412ed42088d881fecbdd1fbebb80dd4caf
yakyab <jablonskijakubit@gmail.com>
2023-09-13 15:58:49 +0200

    Dodanie View Modeli i podstawowego inferfejsu

---
+++ WirtualnyOgrod/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WirtualnyOgrod.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WirtualnyOgrod/ViewModels/PlantViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WirtualnyOgrod.Models;
+
+namespace WirtualnyOgrod.ViewModels
+{
+    public class PlantViewModel : BaseViewModel
+    {
+        private Plant _currentPlant;
+        public Plant CurrentPlant
+        {
+            get { return _currentPlant; }
+            set
+            {
+                _currentPlant = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public PlantViewModel()
+        {
+            CurrentPlant = new Plant();
+        }
+    }
+}
